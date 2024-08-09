767c7ff374a56058d6eed70265f32d80d9cc47dd
Maksym <maksimbondar225@gmail.com>
2024-08-09 13:45:22 +0200

    Game Loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -11,10 +11,45 @@ public class GamePanel extends JPanel{
 	
 	public static final int WIDTH = 1100;
 	public static final int HEIGHT = 800;
+	final int FPS = 60;
+	Thread gameThread;
+	Runnable runnable;
 	
 	GamePanel() {
 		setPreferredSize(new Dimension(WIDTH,HEIGHT));
 		setBackground(Color.black);
+	}
+	
+	public void launchGame() {
+		runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				// Game loop
+				// Calls update() and repaint() 60 times per second
+				double drawInterval = 1000000000/FPS;
+				double delta = 0;
+				long lastTime = System.nanoTime();
+				long currentTime;
+				
+				while (gameThread != null) {
+					currentTime = System.nanoTime();
+					
+					delta += (currentTime - lastTime) / drawInterval;
+					lastTime = currentTime;
+					
+					if (delta >= 1) {
+						update();
+						repaint();
+						delta--;
+					}
+				}
+			}
+			
+		};
+		
+		gameThread = new Thread(runnable);
+		gameThread.start();
 	}
 	
 	private void update() {
@@ -24,4 +59,5 @@ public class GamePanel extends JPanel{
 	public void paintComponent( Graphics g) {
 		super.paintComponent(g);
 	}
+
 }
